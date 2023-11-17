1c33175a333883b84ddad92e1500acaae0a8941b
Diego Serrano <diegoserranor@gmail.com>
2023-11-17 13:23:58 -0500

    fix: separate run and cleanup routines

--- admin/wayru-os-services/source/main.c
+++ admin/wayru-os-services/source/main.c
@@ -8,6 +8,7 @@ struct ScheduledTask {
     time_t nextExecutionTime;
     void (*task)();
     int interval;
+    int isDone;
 };
 
 typedef struct ScheduledTask ScheduledTask;
@@ -27,6 +28,7 @@ void scheduleAt(Scheduler* sch, time_t time, void (*task)()) {
     sch->tasks[sch->size - 1].nextExecutionTime = time;
     sch->tasks[sch->size - 1].task = task;
     sch->tasks[sch->size - 1].interval = 0;  // No es periódico
+    sch->tasks[sch->size - 1].isDone = 0;
 }
 
 // Programa una tarea para ejecutarse periódicamente con un intervalo dado
@@ -36,49 +38,85 @@ void scheduleEvery(Scheduler* sch, int interval, void (*task)()) {
     sch->tasks[sch->size - 1].nextExecutionTime = time(NULL) + interval;
     sch->tasks[sch->size - 1].task = task;
     sch->tasks[sch->size - 1].interval = interval;
+    sch->tasks[sch->size - 1].isDone = 0;
 }
 
 // Programa una tarea para ejecutarse periódicamente con un intervalo dado
 void run(Scheduler* sch) {
     while (1) {
+        // Ejecuta las tareas a las que les toca ser ejecutadas
         time_t now = time(NULL);
-        for (size_t i = 0; i < sch->size; i++) {
+        size_t i = 0;
+        while(i < sch->size) {
             if (now >= sch->tasks[i].nextExecutionTime) {
                 sch->tasks[i].task();
                 if (sch->tasks[i].interval > 0) {
                     sch->tasks[i].nextExecutionTime += sch->tasks[i].interval;
                 } else {
-                    // Eliminar tarea no periódica
-                    for (size_t j = i; j < sch->size - 1; j++) {
-                        sch->tasks[j] = sch->tasks[j + 1];
-                    }
-                    sch->size--;
-                    sch->tasks = realloc(sch->tasks, sch->size * sizeof(ScheduledTask));
+                    // Marcar la tarea para ser eliminada
+                    sch->tasks[i].isDone = 1;
                 }
             }
+
+            i++; // Pasa a la siguiente tarea
         }
-        usleep(100000);  // Esperar 100 ms
+
+        // Eliminar las tareas que ya fueron ejecutadas
+        i = 0;
+        while(i < sch->size) {
+            if (sch->tasks[i].isDone == 1) {
+                for (size_t j = i; j < sch->size - 1; j++) {
+                    sch->tasks[j] = sch->tasks[j + 1];
+                }
+
+                sch->size--;
+                sch->tasks = realloc(sch->tasks, sch->size * sizeof(ScheduledTask));
+            } else {
+                i++; // Pasa a la siguiente tarea
+            }
+        }
+
+        usleep(100000); // Esperar 100 ms
     }
 }
 
 // Función para ejecutar un script Bash
 void task1() {
-    // Ruta completa al script Bash a ejecutar
-    char* scriptPath = "/etc/scripts/utils/get-id.sh";
-    printf("Ejecutando script 1: %s\n", scriptPath);
+    // // Ruta completa al script Bash a ejecutar
+    // char* scriptPath = "/etc/scripts/utils/get-id.sh";
+    // printf("Ejecutando script 1: %s\n", scriptPath);
 
-    // Ejecutar el script Bash como un comando del sistema
-    char command[100];
-    sprintf(command, "bash %s > /etc/scripts/utils/identifier/id", scriptPath); // Ruta donde se guardará el resultado
-    system(command);
+    // // Ejecutar el script Bash como un comando del sistema
+    // char command[100];
+    // sprintf(command, "bash %s > /etc/scripts/utils/identifier/id", scriptPath); // Ruta donde se guardará el resultado
+    // system(command);
+    printf("ejecutando tarea que solo se deberia ejecutar 1 vez\n");
 }
 
 void task2() {
         printf("--3\n");
 }
 
+void task3() {
+        printf("--5\n");
+}
+
 int main() {
     Scheduler sch = {NULL, 0};
+
+    // Test #1 (mixed)
+    // scheduleAt(&sch, time(NULL) + 4, task1);
+    // scheduleEvery(&sch, 3, task2);
+    // scheduleAt(&sch, time(NULL) + 8, task1);
+    // scheduleEvery(&sch, 5, task2);
+
+    // Test #2 (only periodic)
+    // scheduleEvery(&sch, 3, task2);
+    // scheduleEvery(&sch, 5, task3);
+
+    // Test #3 (only non-periodic)
+    // scheduleAt(&sch, time(NULL) + 4, task1);
+    // scheduleAt(&sch, time(NULL) + 8, task1);
 
     // Programa la tarea 1 para ejecutarse en un tiempo determinado (modificar el tiempo aquí)
     scheduleAt(&sch, time(NULL) + 4, task1); // Ejemplo: 3600 segundos = 1 hora

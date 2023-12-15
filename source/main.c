2b9e09d1568734ac95c4fe1adf4e2d4fa6edeeee
Diego Serrano <diegoserranor@gmail.com>
2023-12-15 16:45:17 -0500

    hotfix: wait so openwisp sets up and assigns id

--- source/main.c
+++ source/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "pthread.h"
 #include "services/init.h"
 #include "services/access.h"
@@ -59,6 +60,8 @@ void *schedulerRoutine(void *arg)
 
 int main(int argc, char *argv[])
 {
+    usleep(30000000);
+
     init(argc, argv);
 
     // testGetRequest();

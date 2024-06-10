d9dd46ce989db6c977145564298c84856831072e
Diego <diegoserranor@gmail.com>
2024-06-10 09:52:45 -0500

    fix: read key content only

--- source/services/did-key.c
+++ source/services/did-key.c
@@ -6,9 +6,12 @@
 #include "services/config.h"
 #include <json-c/json.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define PRIVKEY_FILE_NAME "did_key"
 #define PUBKEY_FILE_NAME "did_key.pub"
+#define HEADER "-----BEGIN PUBLIC KEY-----"
+#define FOOTER "-----END PUBLIC KEY-----"
 
 char *read_did_private_key() {
     console(CONSOLE_DEBUG, "reading did private key");
@@ -34,9 +37,6 @@ char *read_did_private_key() {
 }
 
 char *read_did_public_key() {
-    // 1. Open public key file
-    // 2. Read file contents
-
     console(CONSOLE_DEBUG, "reading did public key");
 
     char pubkey_file_path[256];
@@ -55,8 +55,40 @@ char *read_did_public_key() {
     fread(file_data_str, 1, fsize, file);
     fclose(file);
 
-    console(CONSOLE_DEBUG, file_data_str);
-    return file_data_str;
+    file_data_str[fsize] = '\0';
+
+    // Find the key file headers to remove them later
+    char *header_pos = strstr(file_data_str, HEADER);
+    char *footer_pos = strstr(file_data_str, FOOTER);
+
+    if (header_pos == NULL || footer_pos == NULL) {
+        console(CONSOLE_ERROR, "Invalid key file format");
+        free(file_data_str);
+        return 0;
+    }
+
+    // Move past the header and newlines
+    char *key_start = header_pos + strlen(HEADER);
+    while (*key_start == '\n' || *key_start == '\r') {
+        key_start++;
+    }
+
+    // Move to the position of the footer and backup to remove newlines
+    char *key_end = footer_pos;
+    while (key_end > key_start && (*(key_end - 1) == '\n' || *(key_end - 1) == '\r')) {
+        key_end--;
+    }
+
+    size_t key_length = key_end - key_start;
+
+    char *key_content = malloc(key_length + 1);
+    strncpy(key_content, key_start, key_length);
+    key_content[key_length] = '\0';
+
+    free(file_data_str);
+
+    console(CONSOLE_DEBUG, key_content);
+    return key_content;
 }
 
 void create_did_key_pair() { generate_key_pair(PUBKEY_FILE_NAME, PRIVKEY_FILE_NAME); }

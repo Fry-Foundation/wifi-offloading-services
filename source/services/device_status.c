be52cc1c49d4c34ec42effd2050865d1155f6037
esfxra <diegoserranor@gmail.com>
2024-07-23 23:44:00 -0500

    fix: use http_post in device_status

--- source/services/device_status.c
+++ source/services/device_status.c
@@ -1,6 +1,6 @@
 #include "device_status.h"
 #include "lib/console.h"
-#include "lib/curl_helpers.h"
+#include "lib/http-requests.h"
 #include "lib/scheduler.h"
 #include "services/access.h"
 #include "services/config.h"
@@ -16,56 +16,35 @@ DeviceStatus device_status = Unknown;
 bool on_boot = true;
 
 DeviceStatus request_device_status() {
-    CURL *curl;
-    CURLcode res;
-
-    char *response_buffer = init_response_buffer();
-
-    curl = curl_easy_init();
-
-    if (!curl) {
-        console(CONSOLE_ERROR, "device status curl could not be initialized");
-        free(response_buffer);
-        return Unknown;
-    }
-
-    // Set up as POST request
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);
-
     // Url
     char device_status_url[256];
     snprintf(device_status_url, sizeof(device_status_url), "%s%s", config.main_api, DEVICE_STATUS_ENDPOINT);
-    curl_easy_setopt(curl, CURLOPT_URL, device_status_url);
-
-    // Request headers
-    struct curl_slist *headers = NULL;
-    char public_key_header[1024];
-    snprintf(public_key_header, sizeof(public_key_header), "public_key: %s", access_key.public_key);
-    headers = curl_slist_append(headers, public_key_header);
-    headers = curl_slist_append(headers, "Content-Type: application/json");
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
     // Request body
     json_object *json_body = json_object_new_object();
     json_object_object_add(json_body, "on_boot", json_object_new_boolean(on_boot));
     const char *body = json_object_to_json_string(json_body);
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, body);
-
     console(CONSOLE_DEBUG, "device status request body %s", body);
 
-    // Response callback and buffer
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, save_to_buffer_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_buffer);
+    HttpPostOptions options = {
+        .url = device_status_url,
+        .legacy_key = access_key.public_key,
+        .body_json_str = body,
+    };
 
-    res = curl_easy_perform(curl);
+    HttpResult result = http_post(&options);
 
     json_object_put(json_body);
 
-    if (res != CURLE_OK) {
-        console(CONSOLE_ERROR, "access key curl request failed:", curl_easy_strerror(res));
-        curl_easy_cleanup(curl);
-        curl_slist_free_all(headers);
-        free(response_buffer);
+    if (result.is_error) {
+        console(CONSOLE_ERROR, "failed to request device status");
+        console(CONSOLE_ERROR, "error: %s", result.error);
+        return Unknown;
+    }
+
+    if (result.response_buffer == NULL) {
+        console(CONSOLE_ERROR, "failed to request device status");
+        console(CONSOLE_ERROR, "no response received");
         return Unknown;
     }
 
@@ -73,32 +52,25 @@ DeviceStatus request_device_status() {
     struct json_object *parsed_response;
     struct json_object *device_status;
 
-    parsed_response = json_tokener_parse(response_buffer);
+    parsed_response = json_tokener_parse(result.response_buffer);
     if (parsed_response == NULL) {
         // JSON parsing failed
         console(CONSOLE_ERROR, "failed to parse device status JSON data");
-        curl_easy_cleanup(curl);
-        curl_slist_free_all(headers);
-        json_object_put(parsed_response);
-        free(response_buffer);
+        free(result.response_buffer);
         return Unknown;
     }
 
     if (!json_object_object_get_ex(parsed_response, "deviceStatus", &device_status)) {
         console(CONSOLE_ERROR, "publicKey field missing or invalid");
         json_object_put(parsed_response);
-        curl_easy_cleanup(curl);
-        curl_slist_free_all(headers);
-        free(response_buffer);
+        free(result.response_buffer);
         return Unknown;
     }
 
     int response_device_status = json_object_get_int64(device_status);
 
     json_object_put(parsed_response);
-    curl_easy_cleanup(curl);
-    curl_slist_free_all(headers);
-    free(response_buffer);
+    free(result.response_buffer);
 
     console(CONSOLE_DEBUG, "device status response: %d", response_device_status);
 

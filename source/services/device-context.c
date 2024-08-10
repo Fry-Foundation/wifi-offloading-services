c4c62b0cb913180b398d2d602951957c8f8fe858
esfxra <diegoserranor@gmail.com>
2024-08-10 15:58:28 -0500

    feat: periodic device context task

--- source/services/device-context.c
+++ source/services/device-context.c
@@ -1,6 +1,7 @@
 #include "device-context.h"
 #include "lib/console.h"
 #include "lib/http-requests.h"
+#include "lib/scheduler.h"
 #include "services/access_token.h"
 #include "services/config.h"
 #include "services/registration.h"
@@ -12,12 +13,16 @@
 #define DEVICE_ENDPOINT "devices"
 #define DEVICE_CONTEXT_ENDPOINT "context"
 
-char *request_device_context(Registration *registration, AccessToken *access_token) {
+static DeviceContext *device_context;
+static Registration *registration;
+static AccessToken *access_token;
+
+char *request_device_context(Registration *_registration, AccessToken *_access_token) {
     char url[256];
-    snprintf(url, sizeof(url), "%s/%s/%s/%s", config.accounting_api, DEVICE_ENDPOINT, registration->wayru_device_id,
+    snprintf(url, sizeof(url), "%s/%s/%s/%s", config.accounting_api, DEVICE_ENDPOINT, _registration->wayru_device_id,
              DEVICE_CONTEXT_ENDPOINT);
 
-    HttpGetOptions options = {.url = url, .bearer_token = access_token->token};
+    HttpGetOptions options = {.url = url, .bearer_token = _access_token->token};
 
     HttpResult result = http_get(&options);
     if (result.is_error) {
@@ -35,42 +40,74 @@ char *request_device_context(Registration *registration, AccessToken *access_tok
 }
 
 DeviceContext parse_device_context(const char *device_context_json) {
-    DeviceContext device_context;
-    device_context.site = NULL;
+    DeviceContext _device_context;
+    _device_context.site = NULL;
 
     json_object *json = json_tokener_parse(device_context_json);
     if (json == NULL) {
         console(CONSOLE_ERROR, "failed to parse device context json");
-        return device_context;
+        return _device_context;
     }
 
     json_object *site_json = NULL;
     if (!json_object_object_get_ex(json, "site", &site_json)) {
         console(CONSOLE_ERROR, "failed to get site from device context json");
         json_object_put(json);
-        return device_context;
+        return _device_context;
     }
 
-    device_context.site = strdup(json_object_get_string(site_json));
+    _device_context.site = strdup(json_object_get_string(site_json));
     json_object_put(json);
-    return device_context;
+    return _device_context;
 }
 
 DeviceContext *init_device_context(Registration *registration, AccessToken *access_token) {
-    DeviceContext *device_context = (DeviceContext *)malloc(sizeof(DeviceContext));
-    if (device_context != NULL) {
-        device_context->site = NULL;
+    DeviceContext *_device_context = (DeviceContext *)malloc(sizeof(DeviceContext));
+    if (_device_context != NULL) {
+        _device_context->site = NULL;
     }
 
     char *device_context_json = request_device_context(registration, access_token);
     if (device_context_json == NULL) {
         console(CONSOLE_ERROR, "failed to request device context");
-        return device_context;
+        return _device_context;
+    }
+
+    DeviceContext parsed_device_context = parse_device_context(device_context_json);
+    free(device_context_json);
+    _device_context->site = parsed_device_context.site;
+    console(CONSOLE_DEBUG, "context site %s", _device_context->site);
+    return _device_context;
+}
+
+void device_context_task(Scheduler *sch) {
+    char *device_context_json = request_device_context(registration, access_token);
+    if (device_context_json == NULL) {
+        console(CONSOLE_ERROR, "failed to request device context");
+        return;
     }
 
     DeviceContext parsed_device_context = parse_device_context(device_context_json);
     free(device_context_json);
     device_context->site = parsed_device_context.site;
     console(CONSOLE_DEBUG, "context site %s", device_context->site);
-    return device_context;
+    schedule_task(sch, time(NULL) + config.device_status_interval, device_context_task, "device context");
+}
+
+void device_context_service(DeviceContext *_device_context,
+                            Registration *_registration,
+                            AccessToken *_access_token,
+                            Scheduler *_sch) {
+    device_context = _device_context;
+    registration = _registration;
+    access_token = _access_token;
+    device_context_task(_sch);
+}
+
+void clean_device_context(DeviceContext *device_context) {
+    if (device_context->site != NULL) {
+        free(device_context->site);
+    }
+
+    free(device_context);
 }
--- source/services/device-context.h
+++ source/services/device-context.h
@@ -1,13 +1,19 @@
 #ifndef DEVICE_CONTEXT_H
 #define DEVICE_CONTEXT_H
 
-#include "services/registration.h"
+#include "lib/scheduler.h"
 #include "services/access_token.h"
+#include "services/registration.h"
 
 typedef struct {
     char *site;
 } DeviceContext;
 
 DeviceContext *init_device_context(Registration *registration, AccessToken *access_token);
+void device_context_service(DeviceContext *device_context,
+                            Registration *registration,
+                            AccessToken *access_token,
+                            Scheduler *sch);
+void clean_device_context(DeviceContext *device_context);
 
 #endif // DEVICE_CONTEXT_H

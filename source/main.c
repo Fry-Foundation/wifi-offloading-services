508e4a2464950af561fe92d8d90700e0a6f6b1f1
esfxra <diegoserranor@gmail.com>
2024-08-10 15:43:13 -0500

    feat: device context

--- source/main.c
+++ source/main.c
@@ -1,14 +1,15 @@
 #include "lib/scheduler.h"
 #include "services/access.h"
+#include "services/access_token.h"
 #include "services/accounting.h"
 #include "services/config.h"
+#include "services/device-context.h"
 #include "services/device_info.h"
 #include "services/device_status.h"
 #include "services/monitoring.h"
 #include "services/mqtt-cert.h"
 #include "services/mqtt.h"
 #include "services/registration.h"
-#include "services/access_token.h"
 #include "services/setup.h"
 #include <mosquitto.h>
 #include <stdbool.h>
@@ -23,6 +24,7 @@ int main(int argc, char *argv[]) {
     DeviceInfo *device_info = init_device_info();
     Registration *registration = init_registration(device_info->mac, device_info->model, device_info->brand);
     AccessToken *access_token = init_access_token(registration);
+    DeviceContext *device_context = init_device_context(registration, access_token);
     generate_and_sign_cert();
     struct mosquitto *mosq = init_mqtt(access_token);
 

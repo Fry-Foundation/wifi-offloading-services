92dc8f58bf9abd36194961444dc6488797ead88d
esfxra <diegoserranor@gmail.com>
2024-08-13 17:20:47 -0500

    feat: modify preemptive list with lua scripts on connect and disconnect events

--- source/services/site-clients.c
+++ source/services/site-clients.c
@@ -1,16 +1,31 @@
 #include "site-clients.h"
 #include "lib/console.h"
+#include "lib/script_runner.h"
+#include "mosquitto.h"
+#include "services/config.h"
 #include "services/device-context.h"
 #include "services/mqtt.h"
-#include "mosquitto.h"
 #include <stdio.h>
+#include <stdlib.h>
 
+// @todo parse mac and session parameters from payload
 void connect_callback(struct mosquitto *mosq, const struct mosquitto_message *message) {
     console(CONSOLE_DEBUG, "client connected, payload: %s", (char *)message->payload);
+
+    char script_file[512];
+    snprintf(script_file, sizeof(script_file), "%s/%s add %s %s %s %s %s %s %s", config.scripts_path,
+             "nds-preemptive-list.lua", "00:11:22:33:44:55", "3600", "100", "100", "100", "100", "custom_data");
+    char *output = run_script(script_file);
+    free(output);
 }
 
+// @todo parse mac from payload
 void disconnect_callback(struct mosquitto *mosq, const struct mosquitto_message *message) {
     console(CONSOLE_DEBUG, "client disconnected, payload: %s", (char *)message->payload);
+
+    char script_file[512];
+    snprintf(script_file, sizeof(script_file), "%s/%s remove %s", config.scripts_path, "nds-preemptive-list.lua",
+             "00:11:22:33:44:55");
 }
 
 // @todo configure site mac
